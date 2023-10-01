e1831b72713caf3df8f8ab3c3e43004de99c9b0b
Lakshay <lakshayyadav2006@gmail.com>
2023-10-01 19:26:07 +0530

    .

--- techhope/app/pages/Team/Team.jsx
+++ techhope/app/pages/Team/Team.jsx
@@ -17,7 +17,7 @@ const Team = () => {
 
 			<div className={styles.teamContainer}>
 				<TeamMember name="Lakshay Yadav" position={"Founder & President"} />
-				<TeamMember name="Shayan Abdeali Bakshi" position={"Vice President"} />
+				<TeamMember name="Shayan Abdeali Bakshi" position={"IT Head"} />
 				<TeamMember name="Pranav Negi" position={"Finance Manager"} />
 				<TeamMember name="Anikait Dhasmana" position={"Marketing Head"} />
 				<TeamMember name="Aryaveer Singh" position={"HR Manager"} />
