63078f3929b51cae71c02a2f5dc5782635c4a30a
Lakshay <lakshayyadav2006@gmail.com>
2023-10-04 22:42:51 +0530

    Fixed donation popup on mobile.

--- techhope/app/pages/DonationPopup.jsx
+++ techhope/app/pages/DonationPopup.jsx
@@ -17,7 +17,7 @@ const DonationPopup = () => {
 				Please <strong>donate</strong> on this number: <strong>+91 8448402723</strong>
 			</h1>
             <h1>
-                For <strong>volunteering</strong>, please contact: <strong>+91 8448402723</strong> or email us at <strong>techhopefoundation@gmail.com </strong>
+                For <strong>volunteering</strong>, please contact: <strong>+91 8448402723</strong> or email us at <strong className="email">techhopefoundation@gmail.com </strong>
             </h1>
 			<p>Thank You!</p>
 		</div>
